bd841a3e7a4446c600d8a4e5a32ba82c0f389ff1
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2024-01-10 23:00:07 +0900

    feat : deleting tweets

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,8 +1,15 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+    collection,
+    limit,
+    onSnapshot,
+    orderBy,
+    query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { db } from "../firebase";
 import Tweet from "./Tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export interface ITweet {
     id: string;
@@ -13,32 +20,46 @@ export interface ITweet {
     createAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+`;
 
 export default function Timeline() {
     const [tweets, setTweet] = useState<ITweet[]>([]);
-    const fetchTweets = async () => {
-        const tweetsQuery = query(
-            collection(db, "tweets"),
-            orderBy("createdAt", "desc")
-        );
-        const snapshot = await getDocs(tweetsQuery);
-        const tweets = snapshot.docs.map((doc) => {
-            const { tweet, createAt, userId, username, photo } = doc.data();
-            return {
-                tweet,
-                createAt,
-                userId,
-                username,
-                photo,
-                id: doc.id,
-            };
-        });
-        setTweet(tweets);
-    };
 
     useEffect(() => {
+        let unsubscribe: Unsubscribe | null = null;
+        const fetchTweets = async () => {
+            const tweetsQuery = query(
+                collection(db, "tweets"),
+                orderBy("createdAt", "desc"),
+                // 비용절약을 위해 트윗을 전부 로드하는게 아니라 특정 갯수만큼 제한을 건다
+                limit(25)
+            );
+            unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+                const tweets = snapshot.docs.map((doc) => {
+                    const { tweet, createAt, userId, username, photo } =
+                        doc.data();
+                    return {
+                        tweet,
+                        createAt,
+                        userId,
+                        username,
+                        photo,
+                        id: doc.id,
+                    };
+                });
+                setTweet(tweets);
+            });
+        };
         fetchTweets();
+
+        // 더이상 타임라인 컴포넌트가 사용되지 않을때(유저가 로그아웃, 또는 다른화면을 보고있을때), 아래의 함수(return절)를 호출한다
+        return () => {
+            unsubscribe && unsubscribe();
+        };
     }, []);
     return (
         <Wrapper>
