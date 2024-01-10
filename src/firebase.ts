95fc6e8ea67416726de42674eb4bdd3970c12d34
gaeng <78129949+hyeonkyeong31@users.noreply.github.com>
2024-01-10 13:30:18 +0900

    feat : DB와 Storage에 업로드 하고 download URL 사용해서 둘을 연결

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -12,3 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
